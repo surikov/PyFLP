84517f072056d67f2a0f891f1a019f096cc646e2
Srgy Surkv <sss1024@gmail.com>
2022-11-06 15:42:12 +0300

    dump 1.0.5

---
+++ dumpflp.bat
@@ -0,0 +1,10 @@
+rem good
+rem python dump.py "C:/sss/GitHub/PyFLP/tests/assets/FL 20.8.4.flp"
+rem python dump.py "C:/Program Files/Image-Line/FL Studio 20/Data/Demo projects/Song contests/Alex Krutev - Sea_2.flp"
+rem python dump.py "C:/Program Files/Image-Line/FL Studio 20/Data/Demo projects/Demo songs/Adam Szabo - One Day (Funky Mix)_2.flp"
+rem python dump.py "C:/Program Files/Image-Line/FL Studio 20/Data/Demo projects/Demo songs/Musata - Real Bass (Original Mix).flp"
+python dumpflp.py "C:/Program Files/Image-Line/FL Studio 20/Data/Demo projects/Demo songs/Raubana - LIFE_2.flp"
+rem old
+rem python dump.py "C:/Program Files/Image-Line/FL Studio 20/Data/Demo projects/Demo songs/Chris Bulman & Sarju Jagai - No Escape.flp"
+rem python dump.py "C:/Program Files/Image-Line/FL Studio 20/Data/Demo projects/Demo songs/Raubana - LIFE.flp"
+
---
+++ dumpflp.py
@@ -0,0 +1,206 @@
+import sys
+import importlib.util
+import datetime
+
+import pyflp
+
+
+def json_string(txt: any):
+    if txt is None:
+        return ""
+    else:
+        rr = str(txt)
+        rr = rr.replace("'", '"')
+        rr = rr.replace("\n", " ")
+        rr = rr.replace("\r", " ")
+        rr = rr.replace("\\", "/")
+        # rr=rr.replace("2", "8")
+        return '"' + rr + '"'
+
+
+def json_bool(bb: bool):
+    if bb is None:
+        return "false"
+    else:
+        if bb:
+            return "true"
+        else:
+            return "false"
+
+
+def json_num(nn):
+    if nn is None:
+        return "0"
+    else:
+        return str(nn)
+
+
+def json_date(dd: datetime):
+    if dd is None:
+        return '""'
+    else:
+        return '"' + dd.strftime("%Y.%d.%m %H:%M:%S") + '"'
+
+
+def dump_timemarker(delimiter: str, timemarker:pyflp.arrangement.TimeMarker):
+    print("							" + delimiter + "{")
+    print("								denominator: " + json_num(timemarker.denominator))
+    print("								, name: " + json_string(timemarker.name))
+    print("								, numerator: " + json_num(timemarker.numerator))
+    print("								, position: " + json_num(timemarker.position))
+    print("								, type: " + json_string(timemarker.type))
+    print("							}")
+
+def dump_track_item(delimiter: str, item:pyflp.arrangement.PLItemBase):
+    print("									"+delimiter+"{")
+    print("										data: "+ json_string(item))
+    print("									}")
+
+def dump_track(delimiter: str, track:pyflp.arrangement.Track):
+    print("							" + delimiter + "{")
+    print("								name: " + json_string(track.name))
+    print("								, enabled: " + json_bool(track.enabled))
+    print("								, grouped: " + json_bool(track.grouped))
+    print("								, items: [")
+    delimiter = ""
+    for item in track:
+        dump_track_item(delimiter,item)
+        delimiter =delimiter = ", "
+    print("								]")
+    print("							}")
+
+def dump_arrangement(delimiter: str, arrangement: pyflp.arrangement):
+    print("					" + delimiter + "{")
+    print("					name: " + json_string(arrangement.name))
+    print("					, timemarkers: [")
+    delimiter2 = ""
+    for timemarker in arrangement.timemarkers:
+        dump_timemarker(delimiter2,timemarker)
+        delimiter2 = ", "
+    print("						]")
+    print("					, tracks: [")
+    delimiter2 = ""
+    for track in arrangement.tracks:
+        if len(track)>0:
+            dump_track(delimiter2,track)
+            delimiter2 = ", "
+    print("						]")
+    print("					}")
+
+
+def dump_arrangements(project: pyflp.Project):
+    print("		, arrangements: {")
+    print("			loop_pos: " + json_num(project.arrangements.loop_pos))
+    print(
+        "			, time_signature_beat: "
+        + json_num(project.arrangements.time_signature.beat)
+    )
+    print(
+        "			, time_signature_num: " + json_num(project.arrangements.time_signature.num)
+    )
+    print("			, max_tracks: " + json_num(project.arrangements.max_tracks))
+    print("			, arrangements_len: " + json_string(str(len(project.arrangements))))
+    print("			, arrangements_items: [")
+    delimiter = ""
+    for arrangement in project.arrangements:
+        dump_arrangement(delimiter, arrangement)
+        delimiter = ", "
+    print("				]")
+    print("			}")
+
+def dump_automation(delimiter: str, automation:pyflp.channel.Automation):
+    print("					" + delimiter + "{name: " + json_string(automation) + "}")
+
+def dump_channel(delimiter: str, channel:pyflp.channel):
+     print("					" + delimiter + "{name: " + json_string(channel.name) + "}")
+
+def dump_channel_rack(project: pyflp.Project):
+    print("		, channel_rack: {")
+
+    print("			automations: [")
+    delimiter = ""
+    for automation in project.channels.automations:
+        dump_automation(delimiter,automation)
+        delimiter = ", "
+    print("				]")
+    print("			, fit_to_steps: " + json_num(project.channels.fit_to_steps))
+    print("			, height: " + json_num(project.channels.height))
+    print("			, channels: [")
+    delimiter = ""
+    for channel in project.channels:
+        dump_channel(delimiter,channel)
+        delimiter = ", "
+    print("				]")
+    print("			}")
+
+def dump_insert(delimiter: str, insert:pyflp.mixer.Insert):
+    print("					" + delimiter + "{insert: " + json_string(insert) + "}")
+
+def dump_mixer(project: pyflp.Project):
+    print("		, mixer: {")
+    print("			apdc: " + json_bool(project.mixer.apdc))
+    print("			, max_inserts: " + json_num(project.mixer.max_inserts))
+    print("			, max_slots: " + json_num(project.mixer.max_slots))
+    print("			, inserts: [")
+    delimiter = ""
+    for insert in project.mixer:
+        if insert.name is not None:
+            dump_insert(delimiter,insert)
+            delimiter = ", "
+    print("				]")
+    print("			}")
+
+def dump_pattern(delimiter: str, pattern:pyflp.pattern.Pattern):
+    print("					" + delimiter + "{pattern: " + json_string(pattern) + "}")
+
+def dump_patterns(project: pyflp.Project):
+    print("		, patterns: {")
+    print("			play_cut_notes: " + json_bool(project.patterns.play_cut_notes))
+    print("			, patterns: [")
+    delimiter = ""
+    for pattern in project.patterns:
+        dump_pattern(delimiter,pattern)
+        delimiter = ", "
+    print("				]")
+    print("			}")
+
+def dump_flp(project: pyflp.Project):
+    print("	, flp: {")
+    print("		version: " + json_string(str(project.version)))
+    #dump_arrangements(project)
+    print("		, artists: " + json_string(project.artists))
+    #dump_channel_rack(project)
+    print("		, comments: " + json_string(project.comments))
+    print("		, created_on: " + json_date(project.created_on))
+    print("		, data_path: " + json_string(project.data_path))
+    print("		, format: " + json_num(project.format.value))
+    print("		, genre: " + json_string(project.genre))
+    print("		, licensed: " + json_bool(project.licensed))
+    print("		, licensee: " + json_string(project.licensee))
+    print("		, looped: " + json_bool(project.looped))
+    print("		, main_pitch: " + json_num(project.main_pitch))
+    print("		, main_volume: " + json_num(project.main_volume))
+    #dump_mixer(project)
+    print("		, pan_law: " + json_num(project.pan_law))
+    #dump_patterns(project)
+    print("		, ppq: " + json_num(project.ppq))
+    print("		, show_info: " + json_bool(project.show_info))
+    print("		, tempo: " + json_num(project.tempo))
+    print("		, time_spent: " + json_num(project.time_spent.seconds))
+    print("		, title: " + json_string(project.title))
+    print("		, url: " + json_string(project.url))
+    print("	}")
+
+
+def dump(filepath: str):
+    project = pyflp.parse(filepath)
+    print("{")
+    print("	dump: " + json_string("v1.0.5"))
+    print("	, filepath: " + json_string(filepath))
+    print("	, pyflp: " + json_string(pyflp.__version__))
+    dump_flp(project)
+    print("}")
+
+
+filepath = sys.argv[1]
+dump(filepath)
